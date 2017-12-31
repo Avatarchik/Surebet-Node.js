ec05b53c2bbdb573e5c6ec1e98bef97b03291508
marshygeek <lester0578@gmail.com>
2017-12-31 21:06:14 +0400

    Added marathon loading

--- loading/app/marat.js
+++ loading/app/marat.js
@@ -1,29 +1,45 @@
-const url = 'https://www.marathonbet.co.uk/en/live/favorites';
-const expand = 'td[id*="event-more-view"]';
+const default_network_idle = {waitUntil: 'networkidle', networkIdleTimeout: 50};
 const cookie = {name: "SESSION_KEY", value: "0b27ae50ca964a93b286357bd0b3be28", url: "https://www.marathonbet.co.uk"};
-const details = 'div > table > tbody > tr[class*="market-details"]';
+
+const url = "https://www.marathonbet.co.uk/en/live/favorites";
+const node = "#liveEventsContent";
+
+const expand_event_btns = `td[id*="event-more-view"]`;
+const event_details = `div > table > tbody > tr[class*="market-details"]`;
 
 exports.name = 'marat';
 
 exports.load = async function (page) {
     await page.setCookie(cookie);
-    console.log('cookie');
-
     await page.goto(url);
-    await page.waitForNavigation({waitUntil: 'networkidle'});
-    console.log('goto');
+    await page.waitForNavigation(default_network_idle);
 
-    await page.screenshot({path: exports.name + '-before.png'});
-
-    await page.click(expand);
-    console.log('expand');
-    console.log(await page.url());
-
-    await page.waitForSelector(details);
-    console.log('wait for details');
+    console.log(`${exports.name}: loaded`);
 };
 
 exports.load_events = async function (page) {
-    await page.screenshot({path: exports.name + '.png'});
-    return page.content();
+    let site_data = {events: []};
+    site_data.html = await page.$eval(node, el => el.outerHTML);
+    site_data.map_info = await page.evaluate(() => {
+        return reactData.liveMenuEvents.childs
+    });
+
+    const expand_btns = await page.$$(expand_event_btns);
+    for (const cur_btn of expand_btns) {
+        await page.evaluateHandle(e => e.click(), cur_btn)
+            .then(() => page.waitForNavigation(default_network_idle));
+
+        try {
+            await page.waitForSelector(event_details, {timeout: 3000});
+            let event = await page.$eval(event_details, el => el.outerHTML);
+            site_data.events.push(event);
+
+            await page.evaluateHandle(e => e.click(), cur_btn);
+        } catch (err) {
+            console.log(`timeout waiting for 'event_details'`)
+        }
+    }
+
+    console.log(`${exports.name}: loaded events`);
+    return site_data
 };
