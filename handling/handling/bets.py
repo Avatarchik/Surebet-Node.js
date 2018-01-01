904ec23989d0e17eb69a6588d3ae4c5b47e3b72c
korovkinand <korovkinandg@gmail.com>
2018-01-01 22:33:06 +0400

    Move class's variables to __init__. Del unused commands from travis

--- handling/handling/bets.py
+++ handling/handling/bets.py
@@ -1,26 +1,29 @@
 class CondBet:
-    def __init__(self, cond, v1, v2):
+    def __init__(self, cond, v1, v2) -> None:
         self.cond = cond
         self.v1 = v1
         self.v2 = v2
 
 
 class Bets:
-    part = 0
-    o1, ox, o2, o1x, o12, ox2, = (0.0 for i in range(6))
-    total, ind_total1, ind_total2, hand = ([] for i in range(4))
+    def __init__(self) -> None:
+        self.part = 0
+        self.o1, self.ox, self.o2, self.o1x, self.o12, self.ox2, = (0.0 for i in range(6))
+        self.total, self.ind_total1, self.ind_total2, self.hand = ([] for i in range(4))
 
 
 class Event:
-    def __init__(self, team1, team2, parts):
+    def __init__(self, team1, team2, parts) -> None:
         self.team1 = team1
         self.team2 = team2
         self.parts = parts
 
 
 class Sports:
-    soccer, tennis, hockey, basket, volley = ([] for i in range(5))
+    def __init__(self) -> None:
+        self.soccer, self.tennis, self.hockey, self.basket, self.volley = ([] for i in range(5))
 
 
 class Bookmakers:
-    fonbet, olimp, marat = (Sports() for i in range(3))
+    def __init__(self) -> None:
+        self.fonbet, self.olimp, self.marat = (Sports() for i in range(3))
